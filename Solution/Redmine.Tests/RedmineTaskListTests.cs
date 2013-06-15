7e9ceb3fb43026362604346095a5cbbd2af29145
Dmitry Popov <lemon@yandex.ru>
2013-06-15 16:18:45 +0400

    User ID caching

--- Solution/Redmine.Tests/RedmineTaskListTests.cs
+++ Solution/Redmine.Tests/RedmineTaskListTests.cs
@@ -10,11 +10,20 @@ namespace Redmine.Tests
     [TestFixture]
     public class RedmineTaskListTests
     {
+        string usersXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><users total_count=\"1\" offset=\"0\" limit=\"25\" type=\"array\"><user><id>1</id><login>lemon</login><firstname>Dmitry</firstname><lastname>Popov</lastname><mail>lemon@yandex.ru</mail><created_on>2013-06-13T21:30:03Z</created_on><last_login_on>2013-06-13T22:15:54Z</last_login_on></user></users>";
+        string issuesXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><issues total_count=\"1\" offset=\"0\" limit=\"25\" type=\"array\"><issue><id>1</id><project id=\"2\" name=\"Redmine API Library\"/><tracker id=\"2\" name=\"Feature\"/><status id=\"3\" name=\"Resolved\"/><priority id=\"2\" name=\"Normal\"/><author id=\"1\" name=\"Dmitry Popov\"/><assigned_to id=\"1\" name=\"Dmitry Popov\"/><subject>Parse Redmine API XML</subject><description>Users, projects and issues</description><start_date>2013-06-13</start_date><due_date>2013-06-14</due_date><done_ratio>100</done_ratio><estimated_hours>2</estimated_hours><created_on>2013-06-13T22:10:24Z</created_on><updated_on>2013-06-13T22:10:24Z</updated_on><closed_on>2013-06-14T00:15:03Z</closed_on></issue></issues>";
+
+        [SetUp]
+        public void SetUp()
+        {
+            RedmineTaskList.ClearUserCache();
+        }
+
         [Test]
         public void Get()
         {
             var request = MockRepository.GenerateMock<TestWebRequest>();
-            var response = CreateValidResponseStub();
+            var response = CreateValidResponseStub(usersXml, issuesXml);
             
             WebRequest.RegisterPrefix("test", TestWebRequest.GetCreator(request));
             
@@ -31,7 +40,7 @@ namespace Redmine.Tests
         public void Get_AssertUri()
         {
             var request = MockRepository.GenerateMock<TestWebRequest>();
-            var response = CreateValidResponseStub();
+            var response = CreateValidResponseStub(usersXml, issuesXml);
 
             WebRequest.RegisterPrefix("test", TestWebRequest.GetCreator(request));
             
@@ -40,23 +49,71 @@ namespace Redmine.Tests
             request.Expect(x => x.Headers.Add("", "")).IgnoreArguments().Repeat.Twice();
             request.Expect(x => x.GetResponse()).Return(response).Repeat.Twice();
             
-            var issues = RedmineTaskList.Get("lemon", "Pa$sw0rd", "test://redmine.org/");
+            RedmineTaskList.Get("lemon", "Pa$sw0rd", "test://redmine.org/");
 
             request.VerifyAllExpectations();
         }
 
-        private static WebResponse CreateValidResponseStub()
+        [Test]
+        public void Get_AssertUserIdIsCached()
+        {
+            var request = MockRepository.GenerateMock<TestWebRequest>();
+            var response = CreateValidResponseStub(usersXml, issuesXml, issuesXml);
+
+            WebRequest.RegisterPrefix("test", TestWebRequest.GetCreator(request));
+            
+            request.Expect(x => x.Create(new Uri("test://redmine.org/users.xml"))).Repeat.Once();
+            request.Expect(x => x.Create(new Uri("test://redmine.org/issues.xml?assigned_to_id=1"))).Repeat.Twice();
+            request.Expect(x => x.Headers.Add("", "")).IgnoreArguments().Repeat.Times(3);
+            request.Expect(x => x.GetResponse()).Return(response).Repeat.Times(3);
+            
+            RedmineTaskList.Get("lemon", "Pa$sw0rd", "test://redmine.org/");
+            RedmineTaskList.Get("lemon", "Pa$sw0rd", "test://redmine.org/");
+
+            request.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void Get_AssertUserIdIsCachedWithRespectToUrl()
+        {
+            var request = MockRepository.GenerateMock<TestWebRequest>();
+            var response = CreateValidResponseStub(usersXml, issuesXml, usersXml, issuesXml);
+
+            WebRequest.RegisterPrefix("test", TestWebRequest.GetCreator(request));
+            
+            request.Expect(x => x.Create(new Uri("test://redmine1.org/users.xml"))).Repeat.Once();
+            request.Expect(x => x.Create(new Uri("test://redmine2.org/users.xml"))).Repeat.Once();
+            request.Expect(x => x.Create(new Uri("test://redmine1.org/issues.xml?assigned_to_id=1"))).Repeat.Once();
+            request.Expect(x => x.Create(new Uri("test://redmine2.org/issues.xml?assigned_to_id=1"))).Repeat.Once();
+            request.Expect(x => x.Headers.Add("", "")).IgnoreArguments().Repeat.Times(4);
+            request.Expect(x => x.GetResponse()).Return(response).Repeat.Times(4);
+            
+            RedmineTaskList.Get("lemon", "Pa$sw0rd", "test://redmine1.org/");
+            RedmineTaskList.Get("lemon", "Pa$sw0rd", "test://redmine2.org/");
+
+            request.VerifyAllExpectations();
+        }
+
+        private static WebResponse CreateValidResponseStub(params string[] responseXml)
         {
             var response = MockRepository.GenerateStub<WebResponse>();
-            var usersXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><users total_count=\"1\" offset=\"0\" limit=\"25\" type=\"array\"><user><id>1</id><login>lemon</login><firstname>Dmitry</firstname><lastname>Popov</lastname><mail>lemon@yandex.ru</mail><created_on>2013-06-13T21:30:03Z</created_on><last_login_on>2013-06-13T22:15:54Z</last_login_on></user></users>";
-            var issuesXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><issues total_count=\"1\" offset=\"0\" limit=\"25\" type=\"array\"><issue><id>1</id><project id=\"2\" name=\"Redmine API Library\"/><tracker id=\"2\" name=\"Feature\"/><status id=\"3\" name=\"Resolved\"/><priority id=\"2\" name=\"Normal\"/><author id=\"1\" name=\"Dmitry Popov\"/><assigned_to id=\"1\" name=\"Dmitry Popov\"/><subject>Parse Redmine API XML</subject><description>Users, projects and issues</description><start_date>2013-06-13</start_date><due_date>2013-06-14</due_date><done_ratio>100</done_ratio><estimated_hours>2</estimated_hours><created_on>2013-06-13T22:10:24Z</created_on><updated_on>2013-06-13T22:10:24Z</updated_on><closed_on>2013-06-14T00:15:03Z</closed_on></issue></issues>";
-            var streams = new MemoryStream[] {
-                new MemoryStream(Encoding.UTF8.GetBytes(usersXml)),
-                new MemoryStream(Encoding.UTF8.GetBytes(issuesXml))
-            };
-            var i = 0;
+            var streams = new MemoryStream[responseXml.Length];
+            
+            for (int i = 0; i < streams.Length; i++)
+			{
+                streams[i] = new MemoryStream(Encoding.UTF8.GetBytes(responseXml[i]));
+			}
 
-            response.Stub(x => x.GetResponseStream()).Do((Func<Stream>)(() => streams[i++]));
+            var streamIndex = 0;
+            response.Stub(x => x.GetResponseStream()).Do((Func<Stream>)(() => {
+                
+                if (streamIndex >= streams.Length)
+                {
+                    throw new InvalidOperationException("Response is not stubbed");
+                }
+
+                return streams[streamIndex++];
+            }));
 
             return response;
         }
--- Solution/Redmine/RedmineTaskList.cs
+++ Solution/Redmine/RedmineTaskList.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Redmine
 {
-    public class RedmineTaskList
+    public static class RedmineTaskList
     {
+        private static Dictionary<string, int> UserCache;
+
+        static RedmineTaskList()
+        {
+            UserCache = new Dictionary<string, int>();
+        }
+
+        public static void ClearUserCache()
+        {
+            UserCache.Clear();
+        }
+
         public static RedmineIssue[] Get(string username, string password, string baseUriString)
         {
             var baseUri = new Uri(baseUriString);
@@ -18,12 +31,41 @@ namespace Redmine
 
         private static int FindUserId(string username, string password, Uri baseUri)
         {
+            var cacheKey = GetCacheKey(username, baseUri);
+            var cachedUser = GetCachedUser(cacheKey);
+
+            if (cachedUser.Key == null)
+            {
+                cachedUser = RequestAndCache(username, password, baseUri);
+            }
+
+            return cachedUser.Value;
+        }
+
+        private static KeyValuePair<string, int> RequestAndCache(string username, string password, Uri baseUri)
+        {
+            var cacheKey = GetCacheKey(username, baseUri);
             var uri = new Uri(baseUri, "users.xml");
             var xml = new RedmineWebRequest(username, password, uri).GetResponse();
             var users = RedmineXmlParser.ParseUsers(xml);
             var user = users.FirstOrDefault(x => x.Login.Equals(username));
-            
-            return user != null ? user.Id : 0;
+
+            if (user != null)
+            {
+                UserCache.Add(cacheKey, user.Id);
+            }
+
+            return GetCachedUser(cacheKey);
+        }
+
+        private static string GetCacheKey(string username, Uri baseUri)
+        {
+            return String.Concat(username, '@', baseUri);
+        }
+
+        private static KeyValuePair<string, int> GetCachedUser(string cacheKey)
+        {
+            return UserCache.FirstOrDefault(x => x.Key.Equals(cacheKey));;
         }
     }
 }
