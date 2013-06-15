c21f7189d61d8d6cac26a707a67ee3bff5d7ba8f
Dmitry Popov <lemon@yandex.ru>
2013-06-15 19:22:58 +0400

    Added AssemblyInfo for Redmine library; changed assembly title to RedmineTaskList

--- Solution/Redmine/Properties/AssemblyInfo.cs
+++ Solution/Redmine/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RedmineTaskList")]
+[assembly: AssemblyTitle("Redmine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RedmineTaskList")]
+[assembly: AssemblyProduct("Redmine")]
 [assembly: AssemblyCopyright("© 2013 Dmitry Popov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]   
