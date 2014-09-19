076e2ae0faf68cf72f17803bdfd6092040c006b7
Dmitry Popov <lemon@yandex.ru>
2014-09-20 00:34:19 +0400

    Fixed issue #16

--- Solution/RedmineTaskListPackage/RedmineTask.cs
+++ Solution/RedmineTaskListPackage/RedmineTask.cs
@@ -22,31 +22,42 @@ namespace RedmineTaskListPackage
             Document = issue.ProjectName;
             Line = issue.Id;
             Priority = (TaskPriority)Math.Max(3 - issue.PriorityId, 0);
+            Text = GetFormattedDescription(issue, format);
+        }
 
-            Text = String.Format
-               (Regex.Unescape(format),
-                issue.Id,
-                issue.ProjectId,
-                issue.ProjectName,
-                issue.TrackerId,
-                issue.TrackerName,
-                issue.StatusId,
-                issue.StatusName,
-                issue.PriorityId,
-                issue.PriorityName,
-                issue.AuthorId,
-                issue.AuthorName,
-                issue.AssigneeId,
-                issue.AssigneeName,
-                issue.Subject,
-                issue.Description,
-                issue.StartDate,
-                issue.DueDate,
-                issue.DoneRatio,
-                issue.EstimatedHours,
-                issue.CreationTime,
-                issue.LastUpdateTime,
-                issue.ClosingTime);
+        private static string GetFormattedDescription(RedmineIssue issue, string format)
+        {
+            try
+            {
+                return String.Format
+                    (Regex.Unescape(format),
+                     issue.Id,
+                     issue.ProjectId,
+                     issue.ProjectName,
+                     issue.TrackerId,
+                     issue.TrackerName,
+                     issue.StatusId,
+                     issue.StatusName,
+                     issue.PriorityId,
+                     issue.PriorityName,
+                     issue.AuthorId,
+                     issue.AuthorName,
+                     issue.AssigneeId,
+                     issue.AssigneeName,
+                     issue.Subject,
+                     issue.Description,
+                     issue.StartDate,
+                     issue.DueDate,
+                     issue.DoneRatio,
+                     issue.EstimatedHours,
+                     issue.CreationTime,
+                     issue.LastUpdateTime,
+                     issue.ClosingTime);
+            }
+            catch (FormatException)
+            {
+                return "Invalid task description format";
+            }
         }
     }
 }
